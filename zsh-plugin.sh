06adca2525e2ab6fa72fb58ecd1a240af03fe42a
Mawin Mongkolsawat <winz@winz.lan>
2020-09-17 15:22:13 +0700

    Change filename to zsh-plugin.sh

---
+++ zsh-plugin.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# Install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+
+# Auto Suggestions 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
