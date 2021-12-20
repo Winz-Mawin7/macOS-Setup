1b806a0294d440ad3f897314410bc673f938b811
Winz-Mawin7 <WInz.Mawin7@gmail.com>
2021-12-20 11:06:03 +0700

    Update settings nvm configure

--- settings.sh
+++ settings.sh
@@ -19,7 +19,9 @@ chflags nohidden ~/Library
 
 
 echo '# Make directory for Node Version Manager
-export NVM_DIR=$HOME/.nvm
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 source $(brew --prefix nvm)/nvm.sh' >> ~/.zshrc
 
 
